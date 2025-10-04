9ae60d27e316efc2e5c54a25412742797633302a
merawi <merawiyohannes@gmail.com>
2025-10-04 07:20:05 -0700

    modify debug value

--- nova/settings.py
+++ nova/settings.py
@@ -15,9 +15,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG')
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ["*"]
+if DEBUG:
+    ALLOWED_HOSTS = ["*"]
+else:
+    ALLOWED_HOSTS = [
+        "nova-9mjw.onrender.com",
+    ]
 
 # Application definition
 
